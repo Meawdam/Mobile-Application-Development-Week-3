e6c51c0827091357212e738003bde201f8160b5a
DarkCat <meawdamxza@gmail.com>
2026-08-21 09:35:38 +0700

    Fix task model test assertions and improve formatting

--- frontend/test/model_test.dart
+++ frontend/test/model_test.dart
@@ -3,8 +3,16 @@ import 'package:test/test.dart';
 
 void main() {
   test('1. Create a new test should have new a new task', () {
-    Task task = Task("1", "Cooking", false);
+    Task task = Task('1', 'Cooking', false);
 
-    expect(task.id, 1);
+    expect(task.id, '1');
+    expect(task.title, 'Cooking');
+    expect(task.complete, false);
+  });
+
+  test('2. Get task should return a task', () {
+    Task task = Task('1', 'Cooking', false);
+
+    expect(task.getTask(), '[ ] Cooking');
   });
 }
